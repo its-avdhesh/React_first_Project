82ba762b5531ccc54b1712f52cab554f42428701
its-avdhesh <en22it301026@medicaps.ac.in>
2025-01-17 00:37:06 +0530

    Make Some Updates in files

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,10 +2,11 @@ function App() {
  const username = "Hello this is js"
   return (
     <>
-    <h1>This is our js- {username} </h1>
+    <h1>This is our App.jsx and this is js injection- {username} </h1>
     </>
     
   )
 }
 
 export default App
+
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import New from './NewFunction';
+
+function MyApp() {
+  return (
+    <div>
+      <h1>I am a straight Function coming from main.jsx and initialized in main.jsx too</h1>
+    </div>
+  );
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
- 
+    <New />
+    <MyApp />
+  </StrictMode> // Make sure this is closed properly
+);
